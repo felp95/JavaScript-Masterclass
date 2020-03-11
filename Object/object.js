fa437817ae33bf6e2ec5b1538fb98d1437909360
Felipe Portela da Costa <38477564+felp95@users.noreply.github.com>
2020-03-11 15:05:51 -0300

    Update object.js

--- Object/object.js
+++ Object/object.js
@@ -45,3 +45,62 @@ for (const key in book2) {
 
 console.log(equal)
 
+const obj = [
+  {
+    id: 1,
+    atividade: "Evento",
+    materia: "Matemática",
+    descricao: "Super evento",
+    aluno: "Aluno 1",
+    cor: "#0075bf",
+    data: "2020-03-10T17:00:00.638Z"
+  },
+  {
+    id: 2,
+    atividade: "Evento",
+    materia: "Matemática",
+    descricao: "Evento muito massa",
+    aluno: "Aluno 1",
+    cor: "#0075bf",
+    data: "2020-03-11T18:00:00.638Z"
+  },
+  {
+    id: 3,
+    atividade: "Evento",
+    materia: "Matemática",
+    descricao: "Vol 5 | Equações lineares",
+    aluno: "Aluno 1",
+    cor: "#0075bf",
+    data: "2020-03-11T18:00:00.638Z"
+  },
+  {
+    id: 4,
+    atividade: "Trilha",
+    materia: "Matemática",
+    descricao: "Matemática Vol 5 | Equações lineares",
+    aluno: "Aluno 1",
+    cor: "#FFEB3B",
+    data: "2020-03-13T19:00:00.638Z"
+  },
+  {
+    id: 5,
+    atividade: "Avaliação",
+    materia: "Matemática",
+    descricao: "Matemática Vol 1",
+    aluno: "Aluno 1",
+    cor: "#FF9800",
+    data: "2020-03-10T17:00:00.638Z"
+  }
+];
+
+for (const key in obj) {
+  for (const key2 in obj) {
+    if (obj[key].id !== obj[key2].id) {
+      let equal = true;
+      if (obj[key].data !== obj[key2].data) {
+        equal = false;
+      }
+      console.log(`${obj[key].id} - ${equal}`);
+    }
+  }
+}
